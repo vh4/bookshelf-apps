bb444650bb6bd950892e5e65985dc2cb524641ce
Fathoni waseso jati <35059196+vh4@users.noreply.github.com>
2022-10-19 18:19:03 +0700

    update dari github

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const process = req
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () =>{
     console.log("listening to port 3000")
-})
+});
+
