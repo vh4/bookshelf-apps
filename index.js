234d7d9afd3d5cd8b34da59a48bb9dddf7410ad6
Fathoni waseso jati <35059196+vh4@users.noreply.github.com>
2022-10-19 18:10:15 +0700

    update CD to Heroku

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express()
 const path = require('path')
 
 app.use('/', express.static(path.join(__dirname, 'public')))
-const PORT = 3000
+const PORT = process.env.PORT || 5001;
+
 app.listen(PORT, () =>{
     console.log("listening to port 3000")
 })
